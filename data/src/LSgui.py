144d328c7f158e0d4b7756d7ee0d936d743ded2b
Imaad Nisar <62900453+ImaadNisar@users.noreply.github.com>
2022-05-13 16:28:56 +0100

    Add files via upload

--- data/src/LSgui.py
+++ data/src/LSgui.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from turtle import width
 from LSmain import selectPoints
 from PIL import Image, ImageTk
 import webbrowser
@@ -8,6 +7,7 @@ import popup
 
 def mainWin():
     config = validateSettings()
+    print(config)
     root = tk.Tk()
     root.protocol("WM_DELETE_WINDOW", lambda: quitProgram(root))
     root.geometry("1280x720")
@@ -57,7 +57,7 @@ def mainWin():
 
     calibration = tk.Button(sidebar, text=">> CALIBRATION", fg=color["t"], font=btnfont, bg=color["p"], borderwidth=0, padx=50, cursor="hand2", command=selectPoints, highlightthickness=0)
 
-    settings = tk.Button(sidebar, text=">> SETTINGS", fg=color["t"], font=btnfont, bg=color["p"], borderwidth=0, cursor="hand2", command=lambda: viewSettings(main, color, (f"{w}x{h}")), highlightthickness=0)
+    settings = tk.Button(sidebar, text=">> SETTINGS", fg=color["t"], font=btnfont, bg=color["p"], borderwidth=0, cursor="hand2", command=lambda: viewSettings(main, color, (f"{w}",f"{h}"), config), highlightthickness=0)
 
     bottomleft = tk.Frame(root, bg=color["p"])
 
@@ -90,15 +90,25 @@ def quitProgram(root):
 
 def validateSettings():
 
-    settings = ("theme", "wsize", "hsize", "startup", "minimizeToTray")
-    default = "theme: dark\nwsize: 1280\nhsize: 720\nstartup: False\nminimizeToTray: False"
+    keys = ("theme", "wsize", "hsize", "startup", "minimizeToTray")
+
+    default = "theme: dark\nwsize: 1920\nhsize: 1080\nstartup: False\nminimizeToTray: False"
     valid = True
 
     with open("data/src/settings.txt", "r") as f:
         contents = f.readlines()
-        cur_settings = [entry.split(":")[0] in settings for entry in contents]
-        if len(cur_settings) != len(settings) or False in cur_settings:
+        cur_settings = [entry.split(":")[0] in keys for entry in contents]
+        if len(cur_settings) != len(keys) or False in cur_settings:
             valid = False
+
+        for entry in contents:
+            key, val = entry.split(":")[0].strip(), entry.split(":")[1].strip()
+            if ((key == keys[0] and val.lower() not in("dark", "light")) or 
+            (key in keys[1:3] and not val.isnumeric()) or 
+            (key in keys[3:] and val.lower() not in ("true", "false"))):
+                valid = False
+
+
     if not valid:
        with open("data/src/settings.txt", "w") as f:
            f.write(default)
@@ -109,7 +119,12 @@ def validateSettings():
 
 
 
-def viewSettings(main, color, res):
+def viewSettings(main, color, res, config):
+    print(config)
+
+
+
+    #  create widgets
     mainContent = tk.Frame(main, bg=color["s"])
 
     darkMode = tk.BooleanVar()
@@ -120,8 +135,6 @@ def viewSettings(main, color, res):
         "t": "Enables dark mode - requires restart",
         "s": "Run on startup - requires restart",
         "m": "Minimize to system tray when closed - requires restart" 
-
-
     }
 
     font = ("Roboto Mono Light", 12)
@@ -129,47 +142,77 @@ def viewSettings(main, color, res):
     setTitle = tk.Label(mainContent, text="SETTINGS", font=("Roboto Mono Light", 26, "bold"), bg=color["s"], fg=color["t"])
 
     themeChk = tk.Checkbutton(mainContent, variable=darkMode, onvalue=True, offvalue=False, bg=color["s"], fg=color["t"], text=text["t"], highlightthickness=0, activebackground=color["s"], activeforeground=color["t"], font=font, justify="left", command=lambda: enableSave(save))
-
+    themeChk.variable = darkMode  # prevents garbage collection
+    
+    
     startupChk = tk.Checkbutton(mainContent, variable=startup, onvalue=True, offvalue=False, text=text["s"], bg=color["s"], fg=color["t"], highlightthickness=0, activebackground=color["s"], activeforeground=color["t"], font=font, justify="left", command=lambda: enableSave(save))
+    startupChk.variable = startup
 
     minTrayChk = tk.Checkbutton(mainContent, variable=minTray, onvalue=True, offvalue=False, text=text["m"], bg=color["s"], fg=color["t"], highlightthickness=0, activebackground=color["s"], activeforeground=color["t"], font=font, justify="left", command=lambda: enableSave(save))
+    minTrayChk.variable = minTray
+
+
+    for entry in config:
+        key, val = entry.split(":")[0].strip(), entry.split(":")[1].strip()
+        print(val)
+        if key == "theme" and val == "dark":
+            themeChk.select()
+        elif key == "startup" and val == "True":
+            startupChk.select()
+        elif key == "minimizeToTray" and val == "True":
+            minTrayChk.select()
     
-
+    resFrm = tk.Frame(mainContent, bg=color["s"])
+    wlbl = tk.Label(resFrm, text="Width:", bg=color["s"], font=font, fg=color["t"], highlightthickness=0)
+    wtxt = tk.Entry(resFrm, width=10, bg=color["s"], font=font, fg=color["t"], highlightthickness=0, borderwidth=2, relief="groove")
+    hlbl = tk.Label(resFrm, text="Height:", bg=color["s"], font=font, fg=color["t"], highlightthickness=0)
+    htxt = tk.Entry(resFrm, width=10, bg=color["s"], font=font, fg=color["t"], highlightthickness=0, borderwidth=2, relief="groove")
+    resBtn = tk.Button(resFrm, command=lambda: showRes(wlbl, wtxt, hlbl, htxt), text="text here")
     
-
-    selectedRes = tk.StringVar()
-    selectedRes.set(res)
-    resolution = tk.OptionMenu(mainContent, selectedRes, "1920x1080", "Two", "Three")
-    resolution.config(font=font, bg=color["s"], borderwidth=0, highlightthickness=0, indicatoron=0)
-
+    wtxt.insert(tk.END, res[0])  # insert file res
+    htxt.insert(tk.END, res[1])
 
     img = Image.open("data/images/save.png")
     resize = img.resize((125,50), Image.ANTIALIAS)
     imgscaled = ImageTk.PhotoImage(resize)
 
-    save = tk.Button(mainContent, image=imgscaled, bg=color["s"], borderwidth=0, cursor="hand2", activebackground=color["s"], width=85, height=30, command=lambda: onSave(save))
+    save = tk.Button(mainContent, image=imgscaled, bg=color["s"], borderwidth=0, cursor="hand2", activebackground=color["s"], width=85, height=30, command=lambda: onSave(save), highlightthickness=0)
     save.image = imgscaled
+    #  create widgets
 
+
+
+    #  place on screen
     setTitle.grid(row=0, column=0, sticky="n", padx=(230, 0), pady=(20,0))
     themeChk.grid(row=1, column= 0, sticky="w", padx=(50,0), pady=(75, 25))
     startupChk.grid(row=2, column= 0, sticky="w", padx=(50,0), pady=25)
     minTrayChk.grid(row=3, column= 0, sticky="w", padx=(50,0), pady=25)
-    resolution.grid(row=4, column=0, sticky="w", padx=(50,0), pady=25, ipadx=50, ipady=10)
 
-    print(mainContent.winfo_screenwidth())
+    resFrm.grid(row=4, column=0, sticky="w", padx=(50,0), pady=25)
+    resBtn.grid(row=0, column=0, sticky="w", padx=(10,20), pady=25)
+
+    
 
     
     mainContent.grid(row=0, column=0, sticky="nsew")
+    #  place on screen
 
 
 def enableSave(save):
-    save.grid(row=4, column=1, sticky="e", ipadx=20, ipady=10, padx=(50, 0), pady=(150, 0))
+    save.grid(row=5, column=1, sticky="e", ipadx=20, ipady=10, padx=(50, 0), pady=(150, 0))
 
 
 def onSave(btn):
     #save here
     popup.popUp("Changes Saved", "Changes have been saved.\nPlease restart to apply changes.", 1)
     btn.grid_forget()
+
+
+def showRes(wlbl, wtxt, hlbl, htxt):
+    wlbl.grid(row=0, column=1, sticky="w", padx=(0,5))
+    wtxt.grid(row=0, column=2, sticky="w", padx=(0,20))
+    hlbl.grid(row=0, column=3, sticky="w", padx=(0,5))
+    htxt.grid(row=0, column=4, sticky="w")
 
 
 
